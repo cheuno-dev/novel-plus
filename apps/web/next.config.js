33de0a23f905905c7f32f2df6ea2b9dc753e0696
Christina Cheung <ccheung256@gmail.com>
2024-02-16 16:42:04 -0800

    feat: prepare publishing v0.1.23

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -7,42 +7,6 @@ const nextConfig = {
         destination: "https://github.com/cheuno-dev/novel-plus",
         permanent: true,
       },
-      {
-        source: "/sdk",
-        destination: "https://www.npmjs.com/package/novel",
-        permanent: true,
-      },
-      {
-        source: "/npm",
-        destination: "https://www.npmjs.com/package/novel",
-        permanent: true,
-      },
-      {
-        source: "/svelte",
-        destination: "https://github.com/tglide/novel-svelte",
-        permanent: false,
-      },
-      {
-        source: "/vue",
-        destination: "https://github.com/naveennaidu/novel-vue",
-        permanent: false,
-      },
-      {
-        source: "/vscode",
-        destination:
-          "https://marketplace.visualstudio.com/items?itemName=bennykok.novel-vscode",
-        permanent: false,
-      },
-      {
-        source: "/feedback",
-        destination: "https://github.com/cheuno-dev/novel-plus/issues",
-        permanent: true,
-      },
-      {
-        source: "/deploy",
-        destination: "https://vercel.com/templates/next.js/novel",
-        permanent: true,
-      },
     ];
   },
   productionBrowserSourceMaps: true,
